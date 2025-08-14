4cdca8a8d6c7df6f5fbe76fa32267b2578601f53
MatG <mathieugosse13@gmail.com>
2025-08-14 12:47:23 +0200

    🐛 Fix Books typing

--- frontoffice/app/entities/AbstractEntity.ts
+++ frontoffice/app/entities/AbstractEntity.ts
@@ -1,4 +1,5 @@
 export abstract class AbstractEntity {
+    public '@id'!: string
     public id!: number | string;
     public createdAt?: Date;
     public updatedAt?: Date;
--- frontoffice/app/entities/Books.ts
+++ frontoffice/app/entities/Books.ts
@@ -1,11 +1,13 @@
-import type {Authors} from "~/entities/Authors";
 import {AbstractEntity} from "~/entities/AbstractEntity";
+import type {Authors} from "~/entities/Authors";
+import type {Categories} from "~/entities/Categories";
 
 export class Books extends AbstractEntity{
     public name!: string;
-    public Author!: Authors;
+    public author!: Authors | string;
+    public categories!: Categories[] | string[];
 
-    public constructor(data: Partial<Authors>) {
+    public constructor(data: Partial<Books>) {
         super();
         Object.assign(this, data);
     }
