53d9ae952feed8737b389b23023ef479cec2dc80
MatG <mathieugosse13@gmail.com>
2025-08-14 10:42:48 +0200

    ✨ create user & registration controller

---
+++ api/src/Controller/RegistrationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+final class RegistrationController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+    ){
+        $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+    }
+
+    public function __invoke(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // get request body
+        $data = $request->getContent();
+        try {
+            /** @var User $user */
+            $user = $this->serializer->deserialize($data, User::class, 'json');
+        } catch (\Exception $e) {
+            return $this->json(['error' => 'Invalid JSON data'], Response::HTTP_BAD_REQUEST);
+        }
+
+        // check if user valid
+        $errors = $this->validator->validate($user);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+            return $this->json(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
+        }
+
+        // hash password
+        $plaintextPassword = $user->getPassword();
+        $hashedPassword = $passwordHasher->hashPassword($user, $plaintextPassword);
+        $user->setPassword($hashedPassword);
+
+        //save user in db
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        // return response to user
+        return $this->json([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+        ], JsonResponse::HTTP_CREATED);
+    }
+}
--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
+use App\Controller\RegistrationController;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -10,6 +13,16 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ApiResource(
+    operations: [
+        new Post(
+            uriTemplate: '/registration',
+            controller: RegistrationController::class,
+            security: 'is_anonymous()',
+            name: 'registration'
+        )
+    ]
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
